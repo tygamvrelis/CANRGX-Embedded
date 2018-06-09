0e439c8f3dd8042dec9e8a3505216850ebf447fc
linhz0hz <linhz0hz@outlook.com>
2018-06-09 17:03:47 -0400

    Seperate data-logging structure into another class and use binary logging

--- PC-side/canrgx_data_log.py
+++ PC-side/canrgx_data_log.py
@@ -14,33 +14,9 @@ import os
 import numpy as np
 import struct
 from datetime import datetime
-
-def decode_func(raw_bytes):
-    header=struct.unpack('<H',raw_bytes[0:2])[0]
-    time_stamp=struct.unpack('<I',raw_bytes[2:6])[0]
-    acc_x=struct.unpack('<f',raw_bytes[6:10])[0]
-    acc_y=struct.unpack('<f',raw_bytes[10:14])[0]
-    acc_z=struct.unpack('<f',raw_bytes[14:18])[0]
-    mag_x=struct.unpack('<f',raw_bytes[18:22])[0]
-    mag_y=struct.unpack('<f',raw_bytes[22:26])[0]
-    mag_z=struct.unpack('<f',raw_bytes[26:30])[0]
-    mag_1_duty_cycle=struct.unpack('<H',raw_bytes[30:32])[0]
-    mag_2_duty_cycle=struct.unpack('<H',raw_bytes[32:34])[0]
-    tec_1_duty_cycle=struct.unpack('<H',raw_bytes[34:36])[0]
-    tec_2_duty_cycle=struct.unpack('<H',raw_bytes[36:38])[0]
-    temperature_1=struct.unpack('<H',raw_bytes[38:40])[0]
-    temperature_2=struct.unpack('<H',raw_bytes[40:42])[0]
-    temperature_3=struct.unpack('<H',raw_bytes[42:44])[0]
-    temperature_4=struct.unpack('<H',raw_bytes[44:46])[0]
-    temperature_5=struct.unpack('<H',raw_bytes[46:48])[0]
-    temperature_6=struct.unpack('<H',raw_bytes[48:50])[0]
+from canrgx_data_log_file import canrgx_log_files
     
-    return list([header, time_stamp, acc_x, acc_y, acc_z, mag_x, mag_y, mag_z, \
-                mag_1_duty_cycle, mag_2_duty_cycle, tec_1_duty_cycle, \
-                tec_2_duty_cycle, temperature_1, temperature_2, temperature_3, \
-                temperature_4, temperature_5, temperature_6])
-    
-def receive_stream_loop():
+def receive_stream_loop(canrgx_log):
     num_frame_shifts = 0
     num_receptions = 0
 
@@ -56,30 +32,21 @@ def receive_stream_loop():
             # f.write("\n")
             
             # Log unpacked data
-            l = decode_func(raw_data)
-            f.write(datetime.now().strftime('%H.%M.%S.%f') + " ")
-            for item in l:
-                f.write(str(item) + "\t")
-            f.write("\n")
+            header = canrgx_log.decode_data(raw_data)
+            #f.write(datetime.now().strftime('%H.%M.%S.%f') + " ")
+            #for item in l:
+            #    f.write(str(item) + "\t")
+            #f.write("\n")
             
-            # Every so often, we want to make sure our data is written to disk
-            num_receptions = num_receptions + 1
-            if(num_receptions % 1000 == 0):
-                f.flush() # flushes writes to the disk buffer
-                #os.fsync() # if we really wanna make sure the writes persist, this will flush the disk buffer. Slow though
-            
-            # Every so often, write results to terminal as sanity check, etc
-            if(num_receptions % 100 == 0):
-                print("%d, %f, %f" % (l[1], l[4], l[7]))
-                
-            header=struct.unpack('<H',raw_data[0:2])[0]
+            #header=struct.unpack('<H',raw_data[0:2])[0]
             if(header != 65535):
                 # Did not receive expected header "0xFF 0xFF"
                 num_frame_shifts = num_frame_shifts + 1
         except:
             ser.close()
     return num_frame_shifts
-            
+    
+
 def printAndLogStringFromSerial(userMsg=""):
     msg = ser.readline().decode("utf-8")[:-1] # Don't log the newline
     theString = datetime.now().strftime('%H.%M.%S.%f') + " " + userMsg + msg
@@ -104,6 +71,7 @@ if __name__ == "__main__":
     with serial.Serial('COM3',230400,timeout=100) as ser:
         logString("Opened port " + ser.name)
         with open(data_root + time.strftime('%Y_%m_%d_%H_%M_%S') + ".txt", 'w') as f:
+            canrgx_log=canrgx_log_files(data_root)
             # Wait for microcontroller to come on and send its startup message
             printAndLogStringFromSerial("MCU sent: ")
             sendToMCU('A') # ACK
@@ -123,7 +91,7 @@ if __name__ == "__main__":
             sendToMCU('A') # ACK
             
             # Log data in a loop
-            num_frame_shifts = receive_stream_loop()
+            num_frame_shifts = receive_stream_loop(canrgx_log)
             
             # Once MCU is unplugged, we write out all remaining data and close the file
             f.write("Data collection terminated. Number of frame shifts: %d" % num_frame_shifts)
---
+++ PC-side/canrgx_data_log_file.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+CANRAGX Data binary utility.
+Flush data received from serial to a binary file.
+
+Created on Sat Jun  9 15:54:56 2018
+
+@author: linhz
+"""
+import numpy as np
+import time
+
+class canrgx_log_files:
+    
+    def __init__(self,data_root,max_n=500000):
+        self.data_root=data_root
+        self.max_n=max_n
+
+        self.tic_record = np.lib.format.open_memmap(self.data_root + "tic.npy", dtype=np.uint32,  mode='w+', shape=(self.max_n  ,2))
+        #MCU time, PC time
+        self.imu_record = np.lib.format.open_memmap(self.data_root + "imu.npy", dtype=np.float32, mode='w+', shape=(self.max_n  ,6))
+        #Accelerometer and Magnometer data, 
+        self.pwr_record = np.lib.format.open_memmap(self.data_root + "pwr.npy", dtype=np.float32, mode='w+', shape=(self.max_n  ,4))
+        #Power of Magnets and TECs
+        self.tmp_record = np.lib.format.open_memmap(self.data_root + "tmp.npy", dtype=np.float32, mode='w+', shape=(self.max_n  ,6))
+        #Temperature Sensor
+        self.syt_record = np.lib.format.open_memmap(self.data_root + "syt.npy", dtype=np.float64, mode='w+', shape=(self.max_n  ,1))
+        self.i=0
+
+    def decode_data(self, raw_bytes):
+        self.tic_record [i,0] = struct.unpack('<H',raw_bytes[ 0: 2])[0] # header
+        self.tic_record [i,1] = struct.unpack('<I',raw_bytes[ 2: 6])[0] # mcu time tic
+
+        self.imu_record [i,0] = struct.unpack('<f',raw_bytes[ 6:10])[0] # acc_x
+        self.imu_record [i,1] = struct.unpack('<f',raw_bytes[10:14])[0] # acc_y
+        self.imu_record [i,2] = struct.unpack('<f',raw_bytes[14:18])[0] # acc_z
+        self.imu_record [i,3] = struct.unpack('<f',raw_bytes[18:22])[0] # mag_x
+        self.imu_record [i,4] = struct.unpack('<f',raw_bytes[22:26])[0] # mag_y
+        self.imu_record [i,5] = struct.unpack('<f',raw_bytes[26:30])[0] # mag_z
+        
+        self.pwr_record [i,0] = struct.unpack('<H',raw_bytes[30:32])[0] # mag_1_duty_cycle
+        self.pwr_record [i,1] = struct.unpack('<H',raw_bytes[32:34])[0] # mag_2_duty_cycle
+        self.pwr_record [i,2] = struct.unpack('<H',raw_bytes[34:36])[0] # tec_1_duty_cycle
+        self.pwr_record [i,3] = struct.unpack('<H',raw_bytes[36:38])[0] # tec_2_duty_cycle
+        
+        self.tmp_record [i,0] = struct.unpack('<H',raw_bytes[38:40])[0] # temperature_1
+        self.tmp_record [i,1] = struct.unpack('<H',raw_bytes[40:42])[0] # temperature_2
+        self.tmp_record [i,2] = struct.unpack('<H',raw_bytes[42:44])[0] # temperature_3
+        self.tmp_record [i,3] = struct.unpack('<H',raw_bytes[44:46])[0] # temperature_4
+        self.tmp_record [i,4] = struct.unpack('<H',raw_bytes[46:48])[0] # temperature_5
+        self.tmp_record [i,5] = struct.unpack('<H',raw_bytes[48:50])[0] # temperature_6
+        
+        self.syt_record [i,0] = time.time() #System time stamp
+
+        self.i+=1 # increment count
+
+        # Every so often, write results to terminal as sanity check, etc
+        if self.i%100=0:
+            self.sanity_check()
+        
+        # Every so often, we want to make sure our data is written to disk
+        if self.i%1000=0:
+            self.flush()
+        # Close to full, create warning, should start the process
+        if self.i>self.n_max*0.8 && i%1000==0:
+            print("OVF WARNING")
+        return self.tic_record [i,0] # Return header
+
+    def sanity_check(self):
+        #Pring data as sanity check
+        print("%d, %f, %f" % (self.imu_record [i,0], self.imu_record [i,2], self.pwr_record [i,0]))
+
+
+    def flush(self):
+        #Flush data to disk
+        self.tic_record.flush()
+        self.imu_record.flush()
+        self.pwr_record.flush()
+        self.tmp_record.flush()
+        self.syt_record.flush()
+        #os.fsync() # if we really wanna make sure the writes persist, this will flush the disk buffer. Slow though
+
