5ac3da1f95628ae2464be294a103dfdbf73147ce
linhz0hz <linhz0hz@outlook.com>
2018-08-01 22:50:26 -0400

    Improve the client slightly

--- PC-side/AircraftPacket/UDPclientdemo.py
+++ PC-side/AircraftPacket/UDPclientdemo.py
@@ -7,9 +7,11 @@ class EchoClientProtocol(asyncio.DatagramProtocol):
         self.message = message
         self.loop = loop
         self.transport = None
+        self.count=0
 
     def connection_made(self, transport):
         self.transport = transport
+        self.count = 0
         print('Connection Made', self.message)
 
     def datagram_received(self, data, addr):
@@ -26,11 +28,16 @@ class EchoClientProtocol(asyncio.DatagramProtocol):
         LinearMotion = np.frombuffer(data, float_type, 6, 64)
 
         print("GPSTime",GPSTime)
-        print("ModeInfo",ModeInfo)
-        print("AttitudeInfo",AttitudeInfo)
-        print("EarthInfo", EarthInfo)
+        #print("ModeInfo",ModeInfo)
+        #print("AttitudeInfo",AttitudeInfo)
+        #print("EarthInfo", EarthInfo)
         print("LinearMotion", LinearMotion)
 
+        self.count+=1
+        print("count", self.count)
+
+        if self.count>500:
+            self.connection_lost(None)
         #print("Close the socket")
         #self.transport.close()
 
@@ -44,17 +51,16 @@ class EchoClientProtocol(asyncio.DatagramProtocol):
         self.loop.stop()
 
 
-bind_ip = '' # All addresses
+bind_ip = '127.0.0.1' # All addresses
 bind_port = 5124
 # Create a socket for receiving
-sock = socket.socket(socket.AF_INET, # Internet
-                    socket.SOCK_DGRAM) # UDP
-sock.bind((bind_ip, bind_port))
-
+#sock = socket.socket(socket.AF_INET, # Internet
+#                    socket.SOCK_DGRAM) # UDP
+#sock.bind((bind_ip, bind_port))
+#print (sock)
 loop = asyncio.get_event_loop()
 message = "Hello World!"
-connect = loop.create_datagram_endpoint(
-    lambda: EchoClientProtocol(message, loop), sock=sock)
+connect = loop.create_datagram_endpoint(lambda: EchoClientProtocol(message, loop),  local_addr=(bind_ip,bind_port))
 transport, protocol = loop.run_until_complete(connect)
 loop.run_forever()
 transport.close()
