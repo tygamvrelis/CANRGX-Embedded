d28f6c03a9cb3d7590e62038be9a1f25a469ae76
linhz0hz <linhz0hz@outlook.com>
2018-08-02 08:32:50 -0400

    UDP client h5df recording

--- PC-side/canrgx_udp_client_thread.py
+++ PC-side/canrgx_udp_client_thread.py
@@ -1,37 +1,65 @@
-import sys
+import sys,time
 
 import asyncio,socket
 import numpy as np
+import h5py
 
 from PyQt5 import QtCore
 
-class EchoClientProtocol(asyncio.DatagramProtocol):
-    def __init__(self, loop):
+class CANRGXUDPProtocol(asyncio.DatagramProtocol):
+
+    def __init__(self, loop, data_fn=None):
+        
+        self.initialize_data_storage(data_fn)
+
         self.loop = loop
         self.transport = None
-        self.count=0
+        self.index=0
+
+    def initialize_data_storage(self, data_fn):
+        if data_fn is None:
+            data_fn = 'CANRGX_data\\udp_data_' + \
+                time.strftime('%Y_%m_%d_%H_%M_%S') + '.hdf5'
+        self.data_fn = data_fn
+        self.h5_file = h5py.File(self.data_fn, 'w')
+
+        self.gps_time_ds=self.h5_file.create_dataset(
+            'GPS_time',(1000,1),'>i',chunks=True,maxshape=(None,1))
+        self.mode_info_ds=self.h5_file.create_dataset(
+            'mode_info', (1000, 2), '>i', chunks=True, maxshape=(None, 2))
+        self.attitude_info_ds = self.h5_file.create_dataset(
+            'attitude_info', (1000, 6), '>f', chunks=True, maxshape=(None, 6))
+        self.earth_info_ds = self.h5_file.create_dataset(
+            'earth_info', (1000, 3), '>d', chunks=True, maxshape=(None, 3))
+        self.linear_motion_ds = self.h5_file.create_dataset(
+            'linear_motion', (1000, 6), '>f', chunks=True, maxshape=(None, 6))
+
 
     def connection_made(self, transport):
         self.transport = transport
-        self.count = 0
         print('Connection Made')
 
-    def datagram_received(self, data, addr):
-        print("Received from: ", addr)
-
-        self.count += 1
+    def check_ds_size(self):
+        for ds in [self.gps_time_ds, self.mode_info_ds, self.attitude_info_ds, self.earth_info_ds, self.linear_motion_ds]:
+            if self.index +250 >= ds.shape[0]:
+                ds.resize(self.index+1000,0)
+                print('Resized')
         
-        double_type = np.dtype('>d')
-        float_type = np.dtype('>f')
-        int_type = np.dtype('>i')
 
-        GPSTime = np.frombuffer(data, double_type, 1, 0)
-        ModeInfo = np.frombuffer(data, int_type, 2, 8)
-        AttitudeInfo = np.frombuffer(data, float_type, 6, 16)
-        EarthInfo = np.frombuffer(data, double_type, 3, 40)
-        LinearMotion = np.frombuffer(data, float_type, 6, 64)
+    def datagram_received(self, data, addr):
 
-        print("GPSTime", GPSTime, "count", self.count)
+        self.gps_time_ds[self.index,:] = np.frombuffer(data, '>d', 1, 0)
+        self.mode_info_ds[self.index, :] = np.frombuffer(data, '>i', 2, 8)
+        self.attitude_info_ds[self.index, :] = np.frombuffer(data, '>f', 6, 16)
+        self.earth_info_ds[self.index, :] = np.frombuffer(data, '>d', 3, 40)
+        self.linear_motion_ds[self.index, :] = np.frombuffer(data, '>f', 6, 64)
+
+        self.index += 1
+        if(self.index%200==0):
+            self.check_ds_size()
+            print("Received from: ", addr, "GPSTime",
+                  self.gps_time_ds[self.index - 1, 0], "index", self.index)
+        
         #print("ModeInfo",ModeInfo)
         #print("AttitudeInfo",AttitudeInfo)
         #print("EarthInfo", EarthInfo)
@@ -75,7 +103,7 @@ class CANRGXUDPClientThread(QtCore.QThread):
         bind_port = 5124
 
         self.loop = asyncio.new_event_loop()
-        connect = self.loop.create_datagram_endpoint(lambda: EchoClientProtocol(self.loop),  local_addr=(bind_ip,bind_port))
+        connect = self.loop.create_datagram_endpoint(lambda: CANRGXUDPProtocol(self.loop),  local_addr=(bind_ip,bind_port))
         self.transport, self.protocol = self.loop.run_until_complete(connect)
         self.loop.run_forever()
         self.transport.close()
@@ -88,7 +116,7 @@ if __name__ == '__main__':
     QtCore.QTimer.singleShot(0, thread.start)
     # QtCore.QTimer.singleShot(1, ccCamThread.timer.start)
     
-    QtCore.QTimer.singleShot(2000, thread.hint_quit)
+    QtCore.QTimer.singleShot(100000, thread.hint_quit)
 
     thread.finished.connect(qApp.quit)
     sys.exit(qApp.exec_())
